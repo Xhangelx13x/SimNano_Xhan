411d2157a80a4af8b888d2904654e834b4e833dd
José Ángel Contreras <35943755+Xhangelx13x@users.noreply.github.com>
2018-05-13 22:37:43 -0500

    Add files via upload

---
+++ P12/Clase/P12_clase2.R
@@ -0,0 +1,26 @@
+binario <- function(d, l) {
+    b <-  rep(FALSE, l)
+    while (l > 0 | d > 0) {
+        b[l] <- (d %% 2 == 1)
+        l <- l - 1
+        d <- bitwShiftR(d, 1)
+    }
+    return(b)
+}
+ 
+decimal <- function(bits, l) {
+    valor <- 0
+    for (pos in 1:l) {
+        valor <- valor + 2^(l - pos) * bits[pos]
+    }
+    return(valor)
+}
+ 
+library(testit)
+l <- 4
+for (i in 0:9) {
+    print(i)
+    b <- binario(i, l)
+    print(b)
+    assert(i == decimal(b, l))
+}
